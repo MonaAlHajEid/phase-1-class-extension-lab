ee0ae3da82e0b387c83e56a0d337025c68f824cc
Mona <monabadee@gmail.com>
2023-11-24 22:32:33 +0300

    Complete  Lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+// test/indexTest.js
+
+const assert = require('assert');
+const { Polygon, Triangle, Square } = require('../index'); // adjust the path accordingly
+
+describe('Polygon', function () {
+  let polygon;
+
+  beforeEach(function () {
+    polygon = new Polygon([3, 4, 5]);
+  });
+
+  it('has a Polygon class', function () {
+    assert.strictEqual(typeof Polygon, 'function');
+  });
+
+  it('has a perimeter getter', function () {
+    assert.strictEqual(polygon.perimeter, 12);
+  });
+
+  // Add more test cases as needed
+});
+
+
